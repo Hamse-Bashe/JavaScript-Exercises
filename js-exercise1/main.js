ff90e32ee37de5e67520fe00355578a04b835081
Hamse-Bashe <enghamse400@gmail.com>
2026-09-19 23:20:20 +0300

    Adding exercise 1

---
+++ js-exercise1/main.js
@@ -0,0 +1,16 @@
+// Creating variables
+let x = 7;
+let y = 3;
+
+// Arithmetic operations
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+// pre increment
+console.log(++x);
+// post increment
+console.log(x++);
+console.log(x);
+
