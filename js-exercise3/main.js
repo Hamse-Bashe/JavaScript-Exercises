9fce8fda96827049e341053021b6a30362675e2c
Hamse-Bashe <enghamse400@gmail.com>
2026-09-20 15:32:38 +0300

    Adding exercise 3

---
+++ js-exercise3/main.js
@@ -0,0 +1,13 @@
+// Exercise 3
+
+// Variables
+let isSunny = true;
+let isWeekend = false;
+
+// and 
+console.log(isSunny && isWeekend); 
+//or
+console.log(isSunny || isWeekend); 
+// not
+console.log(!isSunny);            
+
