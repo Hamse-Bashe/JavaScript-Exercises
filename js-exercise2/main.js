f97c15a8e092453b6df3dff08ff7ed6695193970
Hamse-Bashe <enghamse400@gmail.com>
2026-09-20 15:20:50 +0300

    Adding exercise 2

---
+++ js-exercise2/main.js
@@ -0,0 +1,29 @@
+// Exercise 2
+
+// Variables
+let a = 15;
+let b = "15";
+
+// equal to
+console.log(a==b);
+
+// not equal to
+console.log(a!=b);
+
+// strictly equal to
+console.log(a===b);
+
+// not strictly equal to
+console.log(a!==b);
+
+// greater than
+console.log(a>b);
+
+// less than
+console.log(a<b);
+
+// greater than or equal
+console.log(a>=b);
+
+// less than or equal
+console.log(a<=b);
