24b28624a58c13653bceff062362e97bfa0c39f2
Hamse-Bashe <enghamse400@gmail.com>
2026-09-20 21:53:45 +0300

    Adding exercise 4

---
+++ js-exercise4/main.js
@@ -0,0 +1,19 @@
+// Exercise 3
+
+// Basic Function
+// function add(number1,number2) {
+//     return number1+number2;
+// }        
+
+// console.log(add(3,4));
+// console.log(add(5,7));
+// console.log(add(2,9));
+
+// Function Expression
+const add = function(number1,number2) {
+    return number1+number2;
+}
+
+console.log(add(10,5));
+console.log(add(20,30));
+console.log(add(30,33));
