599c3b18e686bc5557275242a02d3689467247e9
Hamse-Bashe <enghamse400@gmail.com>
2026-09-20 22:05:01 +0300

    Adding exercise 5

---
+++ js-exercise5/main.js
@@ -0,0 +1,10 @@
+// Exercise 5
+
+// Arrow Function
+const add = (number1,number2) => {
+    return number1+number2;
+}
+
+console.log(add(11,16));
+console.log(add(22,30));
+console.log(add(20,50));
