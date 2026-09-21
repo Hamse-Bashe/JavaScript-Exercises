146eef03a9450a5ca2f505658748e5a84df16108
Hamse-Bashe <enghamse400@gmail.com>
2026-09-21 15:22:12 +0300

    Adding exercise 10

---
+++ js-exercise10/main.js
@@ -0,0 +1,24 @@
+// Exercise 10
+
+let library = [
+    {
+        title : "The Productive Muslim",
+        author : "Mohammed Faris",
+        year : 2016
+    },
+    {
+        title : "Think and Grow Rich",
+        author : "Napoleon Hill",
+        year : 1937
+    },
+    {
+        title : "The Psychology of Money",
+        author : "Morgan Housel",
+        year : 2020
+    }
+];
+
+// Title of the first book
+console.log(library[0].title);
+// Author of the second book
+console.log(library[1].author);
