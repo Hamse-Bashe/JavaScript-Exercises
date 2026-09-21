ea1aad890c9f7c43e9f7bfcf3c3ea4a7010d3bcb
Hamse-Bashe <enghamse400@gmail.com>
2026-09-21 14:49:25 +0300

    Adding exercise 8

---
+++ js-exercise8/main.js
@@ -0,0 +1,14 @@
+// Exercise 8
+
+// Creating object
+let car = {
+    make : "Tesla",
+    model : "Tesla Model S",
+    year : 2026
+}
+
+// loging each property
+console.log(car.make);
+console.log(car.model);
+console.log(car["year"]);
+
