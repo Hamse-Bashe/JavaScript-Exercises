88578074e52a6c7e70a77e9a26c432663b2972b0
Hamse-Bashe <enghamse400@gmail.com>
2026-09-21 14:19:23 +0300

    Adding exercise 7

---
+++ js-exercise7/main.js
@@ -0,0 +1,13 @@
+// Exercise 7
+
+// Creating array
+let colors = ["Red","Green","Blue"];
+console.log(colors);
+
+console.log("Before push");
+colors.push("Yellow");
+console.log(colors);
+
+console.log("Before shift");
+colors.shift();
+console.log(colors);
