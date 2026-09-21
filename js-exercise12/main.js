842ff1dea0f26aa6ceff35e6286b3e4574cd756a
Hamse-Bashe <enghamse400@gmail.com>
2026-09-21 18:53:33 +0300

    Adding exercise 12

---
+++ js-exercise12/main.js
@@ -0,0 +1,9 @@
+// Exercise 12
+
+// fruits array
+let fruits = ["Apple","Banana","Cherry"];
+
+// for loop that prints fruits names
+for(let i = 0; i < fruits.length; i++){
+    console.log(fruits[i]);
+}
