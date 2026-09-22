0994660524394a6654e56afc3d9639fe70dbe932
Hamse-Bashe <enghamse400@gmail.com>
2026-09-22 14:58:22 +0300

    Adding exercise 17

---
+++ js-exercise17/main.js
@@ -0,0 +1,15 @@
+// Exercise 17
+
+// Creating temperature variable
+let temperature = 25;
+
+// if, else if , and else statements
+if(temperature < 0){
+    console.log("Very cold")
+}else if(temperature >= 0 && temperature <= 15){
+    console.log("Cold")
+}else if(temperature > 15 && temperature <= 25){
+    console.log("Warm")
+}else{
+    console.log("Hot")
+}
