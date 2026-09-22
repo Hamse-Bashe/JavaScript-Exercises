57550c731c1f5ccf6f3ca8fd781c17e46bbb9825
Hamse-Bashe <enghamse400@gmail.com>
2026-09-22 15:29:27 +0300

    Adding exercise 19

---
+++ js-exercise19/main.js
@@ -0,0 +1,10 @@
+// Exercise 19
+
+// Creating numbers array
+let numbers = [1,2,3,4,5];
+
+// Using forEach to log each number in numbers array
+numbers.forEach((number)=>{
+    console.log(number);
+});
+
