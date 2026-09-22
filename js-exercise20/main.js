e0f685f241e4e51df8787cf38c643a1aa15c10bc
Hamse-Bashe <enghamse400@gmail.com>
2026-09-22 17:03:00 +0300

    Adding exercise 20

---
+++ js-exercise20/main.js
@@ -0,0 +1,9 @@
+// Exercise 20
+
+// Creating fruits array
+let fruits = ["apple","banana","cherry"];
+
+// Using Map to get the length of each fruit
+let fruitLength = fruits.map((fruit)=> fruit +" = "+ fruit.length);
+
+console.log(fruitLength);
