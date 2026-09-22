cbaf15e041d65d0ecf9132a27057e33bfd8c9c30
Hamse-Bashe <enghamse400@gmail.com>
2026-09-22 17:52:45 +0300

    Adding exercise 23

---
+++ js-exercise23/main.js
@@ -0,0 +1,10 @@
+// Exercise 23
+
+// Creating car object
+const car = { make: "Toyota", model: "Corolla", year: 2020 };
+
+// Destructuring car object
+const {make,model} = car;
+
+console.log(make);
+console.log(model);
