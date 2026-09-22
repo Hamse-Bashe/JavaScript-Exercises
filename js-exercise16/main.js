c2fdcc24492dbdbca6d8e0e1bbde5616a8bd3975
Hamse-Bashe <enghamse400@gmail.com>
2026-09-22 14:39:23 +0300

    Adding exercise 16

---
+++ js-exercise16/main.js
@@ -0,0 +1,11 @@
+// Exercise 16
+
+// Creating score variable
+let score = 80;
+
+// if else statement
+if(score >= 50){
+    console.log("You're passed");
+}else{
+    console.log("You're failed.");
+}
