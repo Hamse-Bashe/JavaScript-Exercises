c1863fae35b79002b674744ff3b6fc096e3700b6
Hamse-Bashe <enghamse400@gmail.com>
2026-09-24 15:11:29 +0300

    Adding exercise 32

---
+++ js-exercise32/main.js
@@ -0,0 +1,14 @@
+// Exercise 32
+
+// Selecting  name
+const name = document.querySelector('#myName');
+console.log(name);
+
+// selecting title
+const title = document.querySelector('h3');
+console.log(title);
+
+// selecting buttons
+const button = document.querySelectorAll('.btn');
+console.log(button);
+
